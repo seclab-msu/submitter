4bb9aa0ba2342461343a50a7863f5cd5a21a6a0f
asterite <daniil.sigalov@solidlab.ru>
2017-10-18 17:52:30 +0300

    add flags and replacers for more tasks

--- flag_replacers.py
+++ flag_replacers.py
@@ -1,3 +1,4 @@
+import json
 import shlex
 import subprocess
 from collections import defaultdict
@@ -21,12 +22,24 @@ CONFIG = {
         'db_path': '/var/www/site/userInfoDB.db',
         'pass_table_name': 'paroli',
         'admin_id': '0'
+    },
+    'xxe_indirect': {
+        'container': 'xxe2',
+        'db_path': '/tmp/registrations.db'
+    },
+    'php_xss_sqli_rce': {
+        'cookies_path': '/home/asterite/xss-bot/runner/ibank/cookies.json'
+    },
+    'php_xss_sqli_rce_sql': {
+        'container': 'php-xss-sqli-rce',
+        'mysql_root_password': 'Admin2015'
     }
+
 }
 
 REPLACE_FUNCS = {}
 
-DOCKER_EXEC = shlex.split('docker exec -i -t')
+DOCKER_EXEC = ['docker', 'exec']
 UPDATE_QUERY_TPL = "UPDATE %s SET password = '%s' WHERE id = %s"
 
 
@@ -69,6 +82,45 @@ def replace_flag_in_their_node(task, flag):
     print 'updating flag in their_node using command', update_db
     subprocess.check_call(update_db)
 
+def replace_flag_in_xxe_indirect(task, flag):
+    config = CONFIG['xxe_indirect']
+
+    query = "UPDATE secret SET flag = '%s'" % flag
+    update_db = DOCKER_EXEC + [
+        config['container'], 'sqlite3', config['db_path'], query
+    ]
+    print 'updating flag in bonus xxe_indirect using command', update_db
+    subprocess.check_call(update_db)
+
+def replace_flag_in_php_sqli_rce(task, flag):
+    cookies_path = CONFIG['php_xss_sqli_rce']['cookies_path'];
+
+    with open(cookies_path) as cookies_file:
+        bot_cookies = json.load(cookies_file)
+
+    for cookie in bot_cookies:
+        if cookie['name'] == 'flag':
+            cookie['value'] = flag
+
+    print "updating flag in bot's cookie (internet bank)"
+
+    with open(cookies_path, 'w') as cookies_file:
+        json.dump(bot_cookies, cookies_file, indent=4)
+
+
+def replace_flag_in_php_sqli_rce_sql(task, flag):
+    config = CONFIG['php_xss_sqli_rce_sql']
+    query = "UPDATE important_bank_data set flag = '%s'" % flag
+    update_db = DOCKER_EXEC + [
+        config['container'], 'mysql', '-u', 'root',
+        '-p' + config['mysql_root_password'], 'bank', '-e', query
+    ]
+    print 'updating mysql flag in internet bank using command', update_db
+    subprocess.check_call(update_db)
+
 
 REPLACE_FUNCS['their-sql-mysql'] = replace_flag_in_their_mysql
 REPLACE_FUNCS['their-sql-node'] = replace_flag_in_their_node
+REPLACE_FUNCS['bonus-xxe-example-indirect'] = replace_flag_in_xxe_indirect
+REPLACE_FUNCS['php-xss-sqli-rce'] = replace_flag_in_php_sqli_rce
+REPLACE_FUNCS['php-xss-sqli-rce-sql'] = replace_flag_in_php_sqli_rce_sql
--- flags.sql
+++ flags.sql
@@ -1,2 +1,5 @@
-insert into tasks values ("their-sql-mysql", 0.1, "placeholder_flag1", 'main_');
-insert into tasks values ("their-sql-node", 0.1, "placeholder_flag2", 'main_');
+insert or replace into tasks values ("their-sql-mysql", 0.1, "main__nm4u3298ihgoeresgheoir", 'main_');
+insert or replace into tasks values ("their-sql-node", 0.1, "main__ndldgjirgh3298t9szohfhg438", 'main_');
+insert or replace into tasks values ("php-xss-sqli-rce", 0.3, "main__ifoewhf432hg843hg943hfew", 'main_');
+insert or replace into tasks values ("php-xss-sqli-rce-sql", 0.5, "main__c00l_bl1nd_sql_inject1on_is_t0ught_right", 'main_');
+insert or replace into tasks values ("bonus-xxe-example-indirect", 0.25, "bonus__Gr3at_j0b_find1ng_SQLi_Everywh3re", 'bonus_');
